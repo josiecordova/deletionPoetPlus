50b60f4bd81a34ba2501bcaf45152d1c7570ee82
Josie Cordova <mothbuilt@gmail.com>
2019-01-24 19:09:25 +0000

    experimenting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -109,7 +109,7 @@ Word: <span id="word"></span>
     </body>
 </html>
 
-				  		  <script type="text/javascript" src="{{ URL::asset('js/highlighter.js') }}"></script>
+				  		  <script type="text/javascript" src="{{ URL::asset('asset/js/highlighter.js') }}"></script>
 
 
 <script>
