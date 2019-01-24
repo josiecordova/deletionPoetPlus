8cc9a95c47ffd10a4d71a7afd45e54d3894321a0
Josie Cordova <mothbuilt@gmail.com>
2019-01-24 09:15:43 +0000

    experimenting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,7 +91,7 @@
 					function myFunction() {
 						var grabbed = <?php 
 						
-						$response = 42; //var_dump(json_decode(http_get("https://en.wikipedia.org/api/rest_v1/page/random/summary"))).length - 1; 
+						$response = var_dump(json_decode(http_get("https://en.wikipedia.org/api/rest_v1/page/random/summary"))).length - 1; 
 						echo $response;
 						
 						
