5a8f2b827bde3d812a9f88e823e9472cc54dc7d7
Josie Cordova <mothbuilt@gmail.com>
2019-01-24 18:09:30 +0000

    experimenting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -99,7 +99,7 @@
     </body>
 </html>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.js"
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.slim.js"
   integrity="sha256-fNXJFIlca05BIO2Y5zh1xrShK3ME+/lYZ0j+ChxX2DA="
   crossorigin="anonymous">
   
