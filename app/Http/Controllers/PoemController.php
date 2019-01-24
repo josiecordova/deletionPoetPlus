419e76c2fb975e1cd8228d6f066dea243f528ff2
Josie Cordova <mothbuilt@gmail.com>
2019-01-24 08:30:52 +0000

    experimenting

--- app/Http/Controllers/PoemController.php
+++ app/Http/Controllers/PoemController.php
@@ -10,7 +10,19 @@ class PoemController extends Controller
 {
     public function show($id)
     {
-      $poem = Poem::find($id);
+		$json = http_get ("https://en.wikipedia.org/api/rest_v1/page/random/summary");
+		
+		$fullSummary = var_dump(json_decode($json));
+		
+		$parsedSummary = $fullSummary.length - 1;
+		
+      $poem = $parsedSummary;//Poem::find($id);
       return view('welcome', array('poem' => $poem));
     }
+	
+	/*public function show($id)
+    {
+      $poem = Poem::find($id);
+      return view('welcome', array('poem' => $poem));
+    }*/
 }
