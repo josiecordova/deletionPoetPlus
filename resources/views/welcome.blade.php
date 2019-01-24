342817cd296dadf0abd048b29454f03977c8dbe5
Josie Cordova <mothbuilt@gmail.com>
2019-01-24 18:02:34 +0000

    experimenting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -102,6 +102,10 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.js"
   integrity="sha256-fNXJFIlca05BIO2Y5zh1xrShK3ME+/lYZ0j+ChxX2DA="
   crossorigin="anonymous">
+  
+  </script>
+
+<script>
 		function grabText() {
 			var grabbed = <?php 
 			
