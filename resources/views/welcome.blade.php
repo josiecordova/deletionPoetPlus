d2a99ff91c90ea59e26903acbdc474a74013665c
Josie Cordova <mothbuilt@gmail.com>
2019-01-24 10:02:39 +0000

    experimenting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,9 +92,9 @@
 						var grabbed = <?php 
 						
 						$client = new GuzzleHttp\Client();
-$res = $client->get('https://en.wikipedia.org/api/rest_v1/page/random/summary');//, ['auth' =>  ['user', 'pass']]);
-echo $res->getStatusCode(); // 200
-echo $res->getBody(); // { "type": "User", ....
+						$res = var_dump(json_decode($client->get('https://en.wikipedia.org/api/rest_v1/page/random/summary'))).length - 1;//, ['auth' =>  ['user', 'pass']]);
+						echo $res->getStatusCode(); // 200
+						echo $res->getBody(); // { "type": "User", ....
 
 						//$response = 
 						
