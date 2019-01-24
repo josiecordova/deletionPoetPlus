2b06db758f57a559d90f4b0a158e63894022fab7
Josie Cordova <mothbuilt@gmail.com>
2019-01-24 11:27:52 +0000

    experimenting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,9 +62,8 @@
                 margin-bottom: 30px;
             }
         </style>
-		myFunction();
     </head>
-    <body>
+    <body onload="myFunction()">
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -85,7 +84,7 @@
                     Laravel
                 </div>
 				
-				<div class="body">
+				<div class="text">
 				<p id="grabbedText">doot doot</p>
 
 
